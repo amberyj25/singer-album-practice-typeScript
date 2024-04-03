17b8d99bdbd97aea34144a3af7031cc1acf9624b
yijin <amber20240@gmail.com>
2024-04-03 12:32:22 +0800

    專輯資料呈現

--- src/App.vue
+++ src/App.vue
@@ -10,21 +10,42 @@
         <button type="button" class="btn btn-primary" @click="searchItunes(searchText)">查詢</button>
       </div>
       <div class="album-placeholder">
-        <div>歌手專輯(輸入歌手後呈現ex.蔡依林)</div>
+        <div>歌手專輯(輸入歌手後呈現 ex.五月天)</div>
+      </div>
+
+      <div v-if="!!albums.data.results">
+        <div class="row d-flex justify-content-center filter">
+          <button type="button" class="btn btn-info col-2 mx-2" @click="getCategory('發行日小到大')">發行日(小到大)</button>
+          <button type="button" class="btn btn-warning col-2 mx-2" @click="getCategory('發行日大到小')">發行日(大到小)</button>
+        </div>
+        <div class="overflow-hidden text-center album">
+          <div class="row gy-5">
+            <div v-for="(album, index) in albumsData" :key="index" class="col-3">
+              <AlbumData :album="album"></AlbumData>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent, ref, reactive } from 'vue';
-import type { ItunesTypes } from '@/types/ItunesTypes';
+import { defineComponent, ref, reactive, computed } from 'vue'
+import type { ItunesTypes } from '@/types/ItunesTypes'
+import AlbumData from '@/components/AlbumData.vue'
+type allCategory = string[];
 
 export default defineComponent({
   name: 'App',
+  components: {
+    AlbumData
+  },
   setup () {
     const searchText = ref('');
     const albums = reactive<{ data: ItunesTypes }>({ data: {} });
+    const allCategory = ref<allCategory>(["發行日大到小", "年份"]);
+    const category = ref<string>("");
 
     const searchItunes = async (search: string) => {
       try {
@@ -33,13 +54,39 @@ export default defineComponent({
         albums.data = valueToJSON;
       } catch (err) {
         console.log('catch', err);
-      }
-    }
+      };
+    };
+
+    const getCategory = (categoryItem: string) => {
+      switch (categoryItem) {
+        case "發行日大到小":
+          category.value = "發行日大到小";
+          break;
+        case "發行日小到大":
+          category.value = "發行日小到大";
+          break;
+      };
+    };
+
+    const albumsData = computed(() => {
+      if (!albums.data.results) return;
+
+      if(category.value === "發行日小到大") {
+        return albums.data.results.sort((a, b) => parseInt(a.releaseDate) - parseInt(b.releaseDate));
+      };
+
+      // default || 發行日大到小
+      return albums.data.results.sort((a, b) => parseInt(b.releaseDate) - parseInt(a.releaseDate));
+    });
 
     return {
       searchText,
       albums,
-      searchItunes
+      searchItunes,
+      allCategory,
+      category,
+      getCategory,
+      albumsData
     }
   }
 })
@@ -77,5 +124,12 @@ export default defineComponent({
   justify-content: center;
   align-items: center;
   margin: 5px 0 15px 0;
+}
+
+.album {
+  background-color: #78cee0;
+  padding: 20px; /* 可根據需要調整 padding 大小 */
+  border-radius: 10px; /* 添加圓角邊框 */
+  box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.5); /* 添加陰影效果 */
 }
 </style>
---
+++ src/components/AlbumData.vue
@@ -0,0 +1,54 @@
+<template>
+    <div class="album">
+      <div class="album-name">{{ album.collectionName }}</div>
+      <div class="album-img-box">
+        <img :src="album.artworkUrl100" alt="img" class="album-img"/>
+      </div>
+      <div>{{ year(album.releaseDate) }}年發行</div>
+    </div>
+  </template>
+  
+<script lang="ts">
+import { defineComponent, ref, reactive } from 'vue'
+
+export default defineComponent({
+    name: 'AlbumData',
+    props: {
+      album: {
+        type: Object,
+        required: true
+      }
+    },
+    setup () {
+      const year = (date: string) => {
+        console.log(date)
+        return date.slice(0, 4)
+      }
+      return { year }
+    }
+  })
+  </script>
+  
+  <style>
+  .album {
+    margin: 15px;
+  }
+  .album-name-title {
+    font-size: 16px;
+  }
+  .album-name {
+    height: 28px;
+    font-size: 16px;
+  }
+  .album-img-box {
+    height: 150px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+  .album-img {
+    width: 125px;
+    height: 100px;
+  }
+  </style>
+  
